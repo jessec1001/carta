24ea91729b71e3edf77278be9cbe99e02d683c40
jessec1001 <jessecorralejr@gmail.com>
2021-02-03 17:17:58 -0700

    feat: add jg formatted graph types

--- CartaWeb/ClientApp/src/lib/types/jg-format.ts
+++ CartaWeb/ClientApp/src/lib/types/jg-format.ts
@@ -1,45 +1,45 @@
 /** Represents a graph property of a graph node. */
-interface VisProperty {
-    /** The name of the property. */
-    name: string,
+interface JgProperty {
     /** The value type of the property. */
     type: string,
     /** The value assigned to the property. */
     value: unknown
 }
 
-/** Represents a graph node that can be directly imported into Vis.js. */
-interface VisNode {
-    /** The unique identifier of this node. */
-    id: string,
-
+/** Represents a graph node. */
+interface JgNode {
     /** The label of the node which appears under each node when visualized. */
     label?: string,
     /** The description of the node which appears when each node is hovered over as a popup. */
     title?: string,
 
-    /** The properties that this node is assigned. */
-    properties: Array<VisProperty>
+    /** The metadata containing property values that this node is assigned. */
+    metadata: Record<string, JgProperty>
 }
 
-/** Represents a graph edge that can be directly imported into Vis.js. */
-interface VisEdge {
-    /** The unique identifier of this edge. */
-    id: number,
-    
+/** Represents a graph edge. */
+interface JgEdge {
     /** The unique identifier for the start node of this edge. This order is irrelevant if the graph is not directed. */
-    from: string,
+    source: string,
     /** The unique identifier for the terminal node of this edge. This order is irrelevant if the graph is not directed. */
-    to: string,
+    target: string
 }
 
-/** Represents graph data that can be directly imported into Vis.js. */
-interface VisGraph {
+/** Represents a graph. */
+interface JgGraph {
     /** Whether or not the edges of the graph are directed. */
     directed: boolean,
 
     /** The nodes contained in the graph. */
-    nodes: Array<VisNode>,
+    nodes: Record<string, JgNode>,
     /** The edges contained in the graph. */
-    edges: Array<VisEdge>
+    edges: Array<JgEdge>
+}
+
+/** Represents data that may contain a single graph or multiple graphs. */
+interface JgData {
+    /** Data for a single graph. */
+    graph?: JgGraph,
+    /** Data for multiple graphs. */
+    graphs?: Array<JgGraph>
 }
