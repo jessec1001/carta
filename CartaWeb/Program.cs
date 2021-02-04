a27b4103ca251439b4606bcbdd2b8a612908977b
jessec1001 <jessecorralejr@gmail.com>
2021-02-04 11:44:38 -0700

    docs: add program docs

--- CartaWeb/Program.cs
+++ CartaWeb/Program.cs
@@ -1,21 +1,28 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace CartaWeb
 {
+    /// <summary>
+    /// Represents the entry point of the program.
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Starts the web application.
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
         }
 
+        /// <summary>
+        /// Creates a web host builder using the command line arguments. 
+        /// </summary>
+        /// <param name="args">The command line arguments.</param>
+        /// <returns>The host builder,</returns>
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureLogging(logging =>
