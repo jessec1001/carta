dd66c6bf2fa36d52d7641a222dfa434199fa8111
jessec1001 <jessecorralejr@gmail.com>
2021-02-05 09:12:33 -0700

    chore: remove error handler from production in startup

--- CartaWeb/Startup.cs
+++ CartaWeb/Startup.cs
@@ -75,7 +75,6 @@ namespace CartaWeb
             }
             else
             {
-                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
