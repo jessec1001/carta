3153ed5bbc4ab64ac4991645eabc7114aa3eeaf8
jessec1001 <jessecorralejr@gmail.com>
2021-04-23 06:39:49 -0600

    feat: reimplement statistical actors

---
+++ CartaCore/Workflow/Action/New/Statistical/ActorMean.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using CartaCore.Data;
+using CartaCore.Serialization;
+
+namespace CartaCore.Workflow.Action
+{
+    [DiscriminantDerived("mean")]
+    public class ActorMean : Actor
+    {
+        public override Property TransformProperty(Property property)
+        {
+            try
+            {
+                // Get the numeric data.
+                List<double> numbers = property.Values.Cast<double>().ToList();
+
+                if (numbers.Count > 0)
+                {
+                    // Compute the mean.
+                    double mean = numbers.Average();
+
+                    // Assign the mean.
+                    if (property.Subproperties is null)
+                        property.Subproperties = Enumerable.Empty<Property>();
+                    property.Subproperties = property.Subproperties.Append
+                    (
+                        new Property
+                        (
+                            Identity.Create("Mean"),
+                            new object[] { mean }
+                        )
+                    );
+                }
+            }
+            catch (InvalidCastException) { }
+            return property;
+        }
+    }
+}
