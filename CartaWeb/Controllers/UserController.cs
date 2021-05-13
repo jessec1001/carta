804ff9b43306697fe7741f4a19a47accd698ea15
jessec1001 <jessecorralejr@gmail.com>
2021-05-13 11:19:23 -0600

    docs: add documentation to user controller

--- CartaWeb/Controllers/UserController.cs
+++ CartaWeb/Controllers/UserController.cs
@@ -5,10 +5,20 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CartaWeb.Controllers
 {
+    /// <summary>
+    /// Serves information about the currently authenticated user and allows for actions such as sign-in and sign-out.
+    /// </summary>
     [ApiController]
     [Route("api/[controller]")]
     public class UserController : ControllerBase
     {
+        /// <summary>
+        /// Gets information about the currently authenticated user.
+        /// </summary>
+        /// <request name="Example"></request>
+        /// <returns status="200">
+        /// A dictionary of key-value pairs of user information.
+        /// </returns>
         [Authorize]
         [HttpGet]
         public ActionResult<Dictionary<string, string>> GetUser()
@@ -25,6 +35,13 @@ namespace CartaWeb.Controllers
             return Ok(info);
         }
 
+        /// <summary>
+        /// Attempts to sign-in a user via a challenge if not authenticated. Otherwise, does nothing.
+        /// </summary>
+        /// <request name="Example"></request>
+        /// <returns status="200">
+        /// Sets the appropriate authentication cookies on the client.
+        /// </returns>
         [HttpGet("signin")]
         public IActionResult SignInUser()
         {
@@ -33,6 +50,13 @@ namespace CartaWeb.Controllers
             else
                 return Challenge();
         }
+        /// <summary>
+        /// Attempts to sign-out a user if authenticated.
+        /// </summary>
+        /// <request name="Example"></request>
+        /// <returns status="200">
+        /// Removes the appropriate authentication cookies from the client.
+        /// </returns>
         [HttpGet("signout")]
         public IActionResult SignOutUser()
         {
