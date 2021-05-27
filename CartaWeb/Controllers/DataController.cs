a0b0e932f91cd9fa71964537bf2c25fda23daea0
jessec1001 <jessecorralejr@gmail.com>
2021-05-27 10:49:36 -0600

    feat: Added alias and workspaceId as optional properties to store with the data set, and changed the type of source from string to DataSource

--- CartaWeb/Controllers/DataController.cs
+++ CartaWeb/Controllers/DataController.cs
@@ -96,12 +96,14 @@ namespace CartaWeb.Controllers
         /// </summary>
         /// <param name="source">The data source.</param>
         /// <param name="resource">The data resource.</param>
+        /// <param name="alias">Optional alias, can be an empty string or null</param>
         /// <returns>
         /// The key for the persisted item for the given data set.
         /// </returns>
-        public static string GetDatasetKey(string source, string resource)
+        public static string GetDatasetKey(string source, string resource, string alias)
         {
-            return "DATASET#" + source + resource;
+            if (alias is null) alias = "";
+            return "DATASET#" + source + resource + alias;
         }
 
 
@@ -443,10 +445,19 @@ namespace CartaWeb.Controllers
         /// <param name="workspaceId">The workspace identifier.</param>
         /// <param name="source">The data set source.</param>
         /// <param name="resource">The data set resource.</param>
+        /// <param name="alias">Optional alias for the data set.</param>
+        /// <param name="workflowId">Optional workflow identifier to apply to the data set.</param>
         /// <request name="Example">
         ///     <arg name="workspaceId">01F68ES7FSMMY1PYCG72B31759</arg>
         ///     <arg name="source">Synthetic</arg>
         ///     <arg name="resource">Finite</arg>
+        /// </request>
+        /// <request name="Example with optional parameters">
+        ///     <arg name="workspaceId">01F68ES7FSMMY1PYCG72B31759</arg>
+        ///     <arg name="source">Synthetic</arg>
+        ///     <arg name="resource">Finite</arg>
+        ///     <arg name="alias">MyDatasetAlias</arg>
+        ///     <arg name="workflowId">01F6Q75NC7TEPXWSNSXJC18BDE</arg>
         /// </request>
         /// <returns status="200">Occurs when the operation is successful. The
         /// data set information will be attached to the response.</returns>
@@ -454,23 +465,27 @@ namespace CartaWeb.Controllers
         [HttpPatch("workspace/{workspaceId}/{source}/{resource}")]
         public async Task<ActionResult<DatasetItem>> PatchWorkspaceData(
             [FromRoute] string workspaceId,
-            [FromRoute] string source,
-            [FromRoute] string resource
+            [FromRoute] DataSource source,
+            [FromRoute] string resource,
+            [FromQuery(Name = "alias")] string alias,
+            [FromQuery(Name = "workflowId")] string workflowId
         )
         {
             DatasetItem datasetItem = new DatasetItem(source, resource, User.FindFirstValue(CognitoUsername));
+            if (alias is not null) datasetItem.Alias = alias;
+            if (workflowId is not null) datasetItem.WorkflowId = workflowId;
             String json = JsonSerializer.Serialize<DatasetItem>(datasetItem, JsonOptions);
             await _noSqlDbContext.SaveDocumentStringAsync
             (
                 GetWorkspaceKey(workspaceId),
-                GetDatasetKey(source, resource),
+                GetDatasetKey(source.ToString(), resource, alias),
                 json
             );
             return Ok(datasetItem);
         }
 
         /// <summary>
-        /// Save the data set under the given workspace. 
+        /// Retrieves all the data sets under the given workspace. 
         /// </summary>
         /// <param name="workspaceId">The workspace identifier.</param>
         /// <request name="Example">
@@ -480,19 +495,56 @@ namespace CartaWeb.Controllers
         /// A list of data sets will be attached to the returned object.</returns>
         [Authorize]
         [HttpGet("workspace/{workspaceId}")]
-        public async Task<ActionResult<List<DatasetItem>>> GetWorkspaceData(
+        public async Task<ActionResult<List<DatasetItem>>> GetWorkspaceDatasets(
             [FromRoute] string workspaceId
         )
         {
             string partitionKey = GetWorkspaceKey(workspaceId);
-            string sortKeyPrefix = GetDatasetKey("", "");
+            string sortKeyPrefix = GetDatasetKey("", "", "");
             List<string> jsonStrings = await _noSqlDbContext.LoadDocumentStringsAsync(partitionKey, sortKeyPrefix);
             List<DatasetItem> datasetItems = new() { };
             foreach (string jsonString in jsonStrings)
             {
                 datasetItems.Add(JsonSerializer.Deserialize<DatasetItem>(jsonString, JsonOptions));
             }
-            return datasetItems;
+            return Ok(datasetItems);
+        }
+
+        /// <summary>
+        /// Retrieves data set information for the specified workspace and data set.
+        /// </summary>
+        /// <param name="workspaceId">The workspace identifier.</param>
+        /// <param name="source">The data set source.</param>
+        /// <param name="resource">The data set resource.</param>
+        /// <param name="alias">Alias for the data set, specify if applicable.</param>
+        /// <request name="Example">
+        ///     <arg name="workspaceId">01F68ES7FSMMY1PYCG72B31759</arg>
+        ///     <arg name="source">Synthetic</arg>
+        ///     <arg name="resource">Finite</arg>
+        /// </request>
+        /// <request name="Example with alias">
+        ///     <arg name="workspaceId">01F68ES7FSMMY1PYCG72B31759</arg>
+        ///     <arg name="source">Synthetic</arg>
+        ///     <arg name="resource">Finite</arg>
+        ///     <arg name="alias">MyDatasetAlias</arg>
+        /// </request>
+        /// <returns status="200">Occurs when the operation is successful.
+        /// The data set information will be attached to the returned object.</returns>
+        /// <returns status="404">Occurs when the data set could not be found.</returns>
+        [Authorize]
+        [HttpGet("workspace/{workspaceId}/{source}/{resource}")]
+        public async Task<ActionResult<DatasetItem>> GetWorkspaceDataset(
+            [FromRoute] string workspaceId,
+            [FromRoute] DataSource source,
+            [FromRoute] string resource,
+            [FromQuery(Name = "alias")] string alias
+        )
+        {
+            string partitionKey = GetWorkspaceKey(workspaceId);
+            string sortKey = GetDatasetKey(source.ToString(), resource, alias);
+            string jsonString = await _noSqlDbContext.LoadDocumentStringAsync(partitionKey, sortKey);
+            if (jsonString is null) return NotFound();
+            else return Ok(JsonSerializer.Deserialize<DatasetItem>(jsonString, JsonOptions));
         }
 
         /// <summary>
@@ -501,19 +553,27 @@ namespace CartaWeb.Controllers
         /// <param name="workspaceId">The workspace identifier.</param>
         /// <param name="source">The data set source.</param>
         /// <param name="resource">The data set resource.</param>
+        /// <param name="alias">Alias for the data set, specify if applicable.</param>
         /// <request name="Example">
         ///     <arg name="workspaceId">01F68ES7FSMMY1PYCG72B31759</arg>
         ///     <arg name="source">Synthetic</arg>
         ///     <arg name="resource">Finite</arg>
         /// </request>
+        /// <request name="Example with alias">
+        ///     <arg name="workspaceId">01F68ES7FSMMY1PYCG72B31759</arg>
+        ///     <arg name="source">Synthetic</arg>
+        ///     <arg name="resource">Finite</arg>
+        ///     <arg name="alias">MyDatasetAlias</arg>
+        /// </request>
         /// <returns status="200">Occurs when the operation is successful.</returns>
         /// <returns status="404">Occurs when the data set could not be found.</returns>
         [Authorize]
         [HttpDelete("workspace/{workspaceId}/{source}/{resource}")]
-        public async Task<ActionResult<DatasetItem>> DeleteWorkspaceData(
+        public async Task<ActionResult> DeleteWorkspaceData(
             [FromRoute] string workspaceId,
-            [FromRoute] string source,
-            [FromRoute] string resource
+            [FromRoute] DataSource source,
+            [FromRoute] string resource,
+            [FromQuery(Name = "alias")] string alias
         )
         {
             DatasetItem datasetItem = new DatasetItem(source, resource, User.FindFirstValue(CognitoUsername));
@@ -521,9 +581,9 @@ namespace CartaWeb.Controllers
             bool deleted = await _noSqlDbContext.DeleteDocumentStringAsync
             (
                 GetWorkspaceKey(workspaceId),
-                GetDatasetKey(source, resource)
+                GetDatasetKey(source.ToString(), resource, alias)
             );
-            if (deleted) return Ok(datasetItem);
+            if (deleted) return Ok();
             else return NotFound();
         }
     }
--- CartaWeb/Models/DocumentItem/DatasetItem.cs
+++ CartaWeb/Models/DocumentItem/DatasetItem.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using CartaWeb.Models.Data;
 
 namespace CartaWeb.Models.DocumentItem
 {
@@ -10,24 +11,32 @@ namespace CartaWeb.Models.DocumentItem
         /// <summary>
         /// The data source
         /// </summary>
-        public string Source { get; set; }
+        public DataSource Source { get; set; }
         /// <summary>
         /// The data resource
         /// </summary>
         public string Resource { get; set; }
         /// <summary>
+        /// Optional alias, to support e.g. an original vs. transformed data set under the same workspace
+        /// </summary>
+        public string Alias { get; set; }
+        /// <summary>
         /// Timestamp of when the data set was added to a worksapce
         /// </summary>
-        public DateTime? DateAdded { get; set; }
+        public DateTime DateAdded { get; set; }
         /// <summary>
         /// The user that added the data set to a workspace
         /// </summary>
         public string AddedBy { get; set; }
+        /// <summary>
+        /// Optional workflow identifier to apply by default to the data set
+        /// </summary>
+        public string WorkflowId { get; set; }
 
         /// <summary>
         /// Creates a new instance of the <see cref="DatasetItem"/> class
         /// </summary>
-        public DatasetItem(string source, string resource, string addedBy)
+        public DatasetItem(DataSource source, string resource, string addedBy)
         {
             Source = source;
             Resource = resource;
