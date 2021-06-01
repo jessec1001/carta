8e29424fa8f2dd53a8befd584ced468b7b6cf2b7
jessec1001 <jessecorralejr@gmail.com>
2021-06-01 11:09:12 -0600

    feat: Added endpoints for persisting data sets under a workspace to the workspace controller.

--- CartaWeb/Controllers/WorkspaceController.cs
+++ CartaWeb/Controllers/WorkspaceController.cs
@@ -11,6 +11,7 @@ using Microsoft.Extensions.Logging;
 
 using CartaCore.Serialization.Json;
 using CartaCore.Persistence;
+using CartaWeb.Models.Data;
 using CartaWeb.Models.DocumentItem;
 
 namespace CartaWeb.Controllers
@@ -78,6 +79,18 @@ namespace CartaWeb.Controllers
         protected static string GetWorkspaceKey(string workspaceId)
         {
             return "WORKSPACE#" + workspaceId;
+        }
+
+        /// <summary>
+        /// Returns a properly formatted data set key.
+        /// </summary>
+        /// <param name="datasetId">A dataest identifier.</param>
+        /// <returns>
+        /// The key for the persisted item for the given dataset.
+        /// </returns>
+        protected static string GetDatasetKey(string datasetId)
+        {
+            return "DATASET#" + datasetId;
         }
 
         /// <summary>
@@ -271,6 +284,23 @@ namespace CartaWeb.Controllers
         }
 
         /// <summary>
+        /// Loads data set information for the given workspace identifier and data set identifier
+        /// </summary>
+        /// <param name="workspaceId">The workspace identifier.</param>
+        /// <param name="datasetId">The dataset identifier</param>
+        protected async Task<DatasetItem> LoadWorkspaceDatasetAsync(
+            string workspaceId,
+            string datasetId
+        )
+        {
+            string partitionKey = GetWorkspaceKey(workspaceId);
+            string sortKey = GetDatasetKey(datasetId);
+            string jsonString = await _noSqlDbContext.LoadDocumentStringAsync(partitionKey, sortKey);
+            if (jsonString is null) return null;
+            else return JsonSerializer.Deserialize<DatasetItem>(jsonString, JsonOptions);
+        }
+
+        /// <summary>
         /// Create a new workspace for the given user.
         /// </summary>
         /// <param name="name">The workspace name.</param>
@@ -417,7 +447,6 @@ namespace CartaWeb.Controllers
         )
         {
             string userId = GetUserId();
-            string userName = GetUserName();
             WorkspaceItem workspaceItem = await LoadWorkspaceItemAsync(userId, id);
             if (workspaceItem is null) return NotFound();
             UserItem userItem = await LoadUserItemAsync(id, userId);
@@ -483,28 +512,28 @@ namespace CartaWeb.Controllers
         /// <summary>
         /// Deletes a user from a workspace. 
         /// </summary>
-        /// <param name="workspaceId">The workspace identifier.</param>
+        /// <param name="id">The workspace identifier.</param>
         /// <param name="users">A list of user identifiers.</param>
         /// <request name="Example">
-        ///     <arg name="workspaceId">01F68ES7FSMMY1PYCG72B31759</arg>
-        ///     <arg name="users">=846817bf-5695-4cca-bd3b-64313beecb56</arg>
+        ///     <arg name="id">01F68ES7FSMMY1PYCG72B31759</arg>
+        ///     <arg name="users">846817bf-5695-4cca-bd3b-64313beecb56</arg>
         /// </request>
         /// <returns status="200">Occurs when the operation is successful.</returns>
         /// <returns status="403">Occurs when the workspace is not owned by the logged in user.</returns>
         /// <returns status="404">Occurs when the user or workspace record could not be found.</returns>
         [Authorize]
-        [HttpDelete("{workspaceId}/users")]
+        [HttpDelete("{id}/users")]
         public async Task<ActionResult> DeleteWorkspaceUser(
-            [FromRoute] string workspaceId,
+            [FromRoute] string id,
             [FromQuery(Name = "users")] List<string> users
         )
         {
             foreach (string userId in users)
             {
                 // Load the workspace and user items
-                WorkspaceItem workspaceItem = await LoadWorkspaceItemAsync(userId, workspaceId);
+                WorkspaceItem workspaceItem = await LoadWorkspaceItemAsync(userId, id);
                 if (workspaceItem is null) return NotFound();
-                UserItem userItem = await LoadUserItemAsync(workspaceId, userId);
+                UserItem userItem = await LoadUserItemAsync(id, userId);
                 if (userItem is null) return NotFound();
 
                 // Delete the workspace for the given user if the workspace is owned by the logged in user
@@ -517,7 +546,7 @@ namespace CartaWeb.Controllers
                 bool deleted = await _noSqlDbContext.DeleteDocumentStringAsync
                 (
                     GetUserKey(userId),
-                    GetWorkspaceKey(workspaceId)
+                    GetWorkspaceKey(id)
                 );
                 if (!deleted)
                 {
@@ -532,7 +561,7 @@ namespace CartaWeb.Controllers
                 string json = JsonSerializer.Serialize<UserItem>(userItem, JsonOptions);
                 bool updated = await _noSqlDbContext.UpdateDocumentStringAsync
                 (
-                    GetWorkspaceKey(workspaceId),
+                    GetWorkspaceKey(id),
                     GetUserKey(userId),
                     json
                 );
@@ -546,6 +575,179 @@ namespace CartaWeb.Controllers
 
             // Return Ok if no errors occurred up to this point
             return Ok();
+        }
+
+        /// <summary>
+        /// Persist the data set under the given workspace. 
+        /// </summary>
+        /// <param name="id">The workspace identifier.</param>
+        /// <param name="source">The data set source.</param>
+        /// <param name="resource">The data set resource.</param>
+        /// <param name="name">Optional name for the data set.</param>
+        /// <request name="Example">
+        ///     <arg name="id">01F68ES7FSMMY1PYCG72B31759</arg>
+        ///     <arg name="source">Synthetic</arg>
+        ///     <arg name="resource">Finite</arg>
+        /// </request>
+        /// <request name="Example with name">
+        ///     <arg name="id">01F68ES7FSMMY1PYCG72B31759</arg>
+        ///     <arg name="source">Synthetic</arg>
+        ///     <arg name="resource">Finite</arg>
+        ///     <arg name="name">MyDatasetName</arg>
+        /// </request>
+        /// <returns status="200">Occurs when the operation is successful. The data set information,
+        /// including an unique identifier, will be attached to the response.</returns>
+        [Authorize]
+        [HttpPost("{id}/data/{source}/{resource}")]
+        public async Task<ActionResult<DatasetItem>> PostWorkspaceData(
+            [FromRoute] string id,
+            [FromRoute] DataSource source,
+            [FromRoute] string resource,
+            [FromQuery(Name = "name")] string name
+        )
+        {
+            DatasetItem datasetItem = new DatasetItem(source, resource, GetUserId());
+            if (name is not null) datasetItem.Name = name;
+            string json = JsonSerializer.Serialize<DatasetItem>(datasetItem, JsonOptions);
+            string datasetId = await _noSqlDbContext.CreateDocumentStringAsync
+            (
+                GetWorkspaceKey(id),
+                GetDatasetKey(""),
+                json
+            );
+            if (datasetId is null) return Conflict();
+            else
+            {
+                datasetItem.Id = datasetId;
+                return Ok(datasetItem);
+            }
+        }
+
+        /// <summary>
+        /// Update the name or workflow of a dataset.
+        /// </summary>
+        /// <param name="id">The workspace identifier.</param>
+        /// <param name="datasetId">The unique data set identifier.</param>
+        /// <param name="name">Optional name for the data set.</param>
+        /// <param name="workflowId">Optional workflow identifier to apply to the data set.</param>
+        /// <request name="Example to update name">
+        ///     <arg name="id">01F68ES7FSMMY1PYCG72B31759</arg>
+        ///     <arg name="datasetId">02F69ES5FSMMY1PYCG72B31531</arg>
+        ///     <arg name="name">MyDataset</arg>
+        /// </request>
+        /// <request name="Example to update workflow">
+        ///     <arg name="id">01F68ES7FSMMY1PYCG72B31759</arg>
+        ///     <arg name="datasetId">02F69ES5FSMMY1PYCG72B31531</arg>
+        ///     <arg name="workflow">01F6Q75NC7TEPXWSNSXJC18BDE</arg>
+        /// </request>
+        /// <request name="Example to update name and workflow">
+        ///     <arg name="id">01F68ES7FSMMY1PYCG72B31759</arg>
+        ///     <arg name="datasetId">02F69ES5FSMMY1PYCG72B31531</arg>
+        ///     <arg name="name">MyDatasetName</arg>
+        ///     <arg name="workflowId">01F6Q75NC7TEPXWSNSXJC18BDE</arg>
+        /// </request>
+        /// <returns status="200">Occurs when the operation is successful. The
+        /// data set information will be attached to the response.</returns>
+        /// <returns status="404">Occurs when the data set could not be found.</returns>
+        [Authorize]
+        [HttpPatch("{id}/data/{datasetId}")]
+        public async Task<ActionResult<DatasetItem>> PatchWorkspaceData(
+            [FromRoute] string id,
+            [FromRoute] string datasetId,
+            [FromQuery(Name = "name")] string name,
+            [FromQuery(Name = "workflow")] string workflowId
+        )
+        {
+            DatasetItem datasetItem = await LoadWorkspaceDatasetAsync(id, datasetId);
+            if (datasetItem is null) return NotFound();
+            if (name is not null) datasetItem.Name = name;
+            if (workflowId is not null) datasetItem.WorkflowId = workflowId;
+            string json = JsonSerializer.Serialize<DatasetItem>(datasetItem, JsonOptions);
+            bool updated = await _noSqlDbContext.UpdateDocumentStringAsync
+            (
+                GetWorkspaceKey(id),
+                GetDatasetKey(datasetId),
+                json
+            );
+            if (!updated) return NotFound();
+            else return Ok(datasetItem);
+        }
+
+        /// <summary>
+        /// Retrieves all the data sets under the given workspace. 
+        /// </summary>
+        /// <param name="id">The workspace identifier.</param>
+        /// <request name="Example">
+        ///     <arg name="workspaceId">01F68ES7FSMMY1PYCG72B31759</arg>
+        /// </request>
+        /// <returns status="200">Occurs when the operation is successful.
+        /// A list of data sets will be attached to the returned object.</returns>
+        [Authorize]
+        [HttpGet("{id}/data")]
+        public async Task<ActionResult<List<DatasetItem>>> GetWorkspaceDatasets(
+            [FromRoute] string id
+        )
+        {
+            string partitionKey = GetWorkspaceKey(id);
+            string sortKeyPrefix = GetDatasetKey("");
+            List<string> jsonStrings = await _noSqlDbContext.LoadDocumentStringsAsync(partitionKey, sortKeyPrefix);
+            List<DatasetItem> datasetItems = new() { };
+            foreach (string jsonString in jsonStrings)
+            {
+                datasetItems.Add(JsonSerializer.Deserialize<DatasetItem>(jsonString, JsonOptions));
+            }
+            return Ok(datasetItems);
+        }
+
+        /// <summary>
+        /// Retrieves data set information for the specified workspace and data set.
+        /// </summary>
+        /// <param name="id">The workspace identifier.</param>
+        /// <param name="datasetId">The data set identifier.</param>
+        /// <request name="Example">
+        ///     <arg name="id">01F68ES7FSMMY1PYCG72B31759</arg>
+        ///     <arg name="datasetId">02F69ES5FSMMY1PYCG72B31531</arg>
+        /// </request>
+        /// <returns status="200">Occurs when the operation is successful.
+        /// The data set information will be attached to the returned object.</returns>
+        /// <returns status="404">Occurs when the data set could not be found.</returns>
+        [Authorize]
+        [HttpGet("{id}/data/{datasetId}")]
+        public async Task<ActionResult<DatasetItem>> GetWorkspaceDataset(
+            [FromRoute] string id,
+            [FromRoute] string datasetId
+        )
+        {
+            DatasetItem datasetItem = await LoadWorkspaceDatasetAsync(id, datasetId);
+            if (datasetItem is null) return NotFound();
+            else return Ok(datasetItem);
+        }
+
+        /// <summary>
+        /// Delete the data set from the given workspace. 
+        /// </summary>
+        /// <param name="id">The workspace identifier.</param>
+        /// <param name="datasetId">The data set identifier.</param>
+        /// <request name="Example">
+        ///     <arg name="id">01F68ES7FSMMY1PYCG72B31759</arg>
+        ///     <arg name="datasetId">02F69ES5FSMMY1PYCG72B31531</arg>
+        /// </request>
+        /// <returns status="200">Occurs when the operation is successful.</returns>
+        /// <returns status="404">Occurs when the data set could not be found.</returns>
+        [Authorize]
+        [HttpDelete("{id}/data/{datasetId}")]
+        public async Task<ActionResult> DeleteWorkspaceData(
+            [FromRoute] string id,
+            [FromRoute] string datasetId
+        )
+        {
+            bool deleted = await _noSqlDbContext.DeleteDocumentStringAsync
+            (
+                GetWorkspaceKey(id),
+                GetDatasetKey(datasetId)
+            );
+            if (deleted) return Ok();
+            else return NotFound();
         }
 
     }
