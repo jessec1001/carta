22073bcceaea5c242b7b03f44692620934a5b2fa
jessec1001 <jessecorralejr@gmail.com>
2021-07-07 16:15:27 -0600

    feat: Added data structures to return information regarding changes made in a workspace

---
+++ CartaWeb/Models/Data/WorkspaceActionEnumeration.cs
@@ -0,0 +1,24 @@
+﻿using CartaWeb.Models.Meta;
+
+namespace CartaWeb.Models.Data
+{
+    /// <summary>
+    /// An enumeration that defines actions taken in a workspace
+    /// </summary>
+    [ApiType(typeof(string))]
+    public enum WorkspaceActionEnumeration
+    {
+        /// <summary>
+        /// An object was added to a workspace
+        /// </summary>
+        Added,
+        /// <summary>
+        /// An object was removed from a workspace
+        /// </summary>
+        Removed,
+        /// <summary>
+        /// An object was updated
+        /// </summary>
+        Updated
+    }
+}
---
+++ CartaWeb/Models/Data/WorkspaceChangeEnumeration.cs
@@ -0,0 +1,28 @@
+﻿using CartaWeb.Models.Meta;
+
+namespace CartaWeb.Models.Data
+{
+    /// <summary>
+    /// An enumeration that defines changes made in a workspace
+    /// </summary>
+    [ApiType(typeof(string))]
+    public enum WorkspaceChangeEnumeration
+    {
+        /// <summary>
+        /// Addition of a workspace
+        /// </summary>
+        Workspace,
+        /// <summary>
+        /// Addition or removal of a user
+        /// </summary>
+        User,
+        /// <summary>
+        /// Addition, update or removal of a workflow
+        /// </summary>
+        Worklow,
+        /// <summary>
+        /// Addition, update or removal of a dataset
+        /// </summary>
+        Dataset
+    }
+}
