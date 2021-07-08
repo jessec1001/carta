659fcd3e70b2cc5b4b8681ee10b24a9ea5b00854
jessec1001 <jessecorralejr@gmail.com>
2021-07-08 11:58:59 -0600

    feat: Added data structures to return information regarding changes made in a workspace

---
+++ CartaWeb/Models/Data/WorkspaceChangeInformation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace CartaWeb.Models.Data
+{
+    /// <summary>
+    /// Encapsulates additional information aboute changes made in a workspace
+    /// </summary>
+    public class WorkspaceChangeInformation
+    {
+
+        /// <summary>
+        /// The workflow ID assigned to a dataset in a workspace
+        /// </summary>
+        public string WorkflowId { get; set; }
+
+        /// <summary>
+        /// The workflow name 
+        /// </summary>
+        public string WorkflowName { get; set; }
+
+        /// <summary>
+        /// The workflow version 
+        /// </summary>
+        public string WorkflowVersion { get; set; }
+
+        /// <summary>
+        /// The dataset name 
+        /// </summary>
+        public string DatasetName { get; set; }
+    }
+}
