8160f1a63c91e58bb47a2593bf5eccbfbba8fda7
jessec1001 <jessecorralejr@gmail.com>
2022-02-03 01:46:04 -0700

    fix: finite undirected graph operation not outputting graph

--- CartaCore/Operations/Concrete/Synthetic/FiniteUndirectedGraphOperation.cs
+++ CartaCore/Operations/Concrete/Synthetic/FiniteUndirectedGraphOperation.cs
@@ -3,11 +3,21 @@ using CartaCore.Data;
 using CartaCore.Integration.Synthetic;
 using CartaCore.Operations.Attributes;
 
+// TODO: Improve support for these synthetic graph generators.
 namespace CartaCore.Operations
 {
+    /// <summary>
+    /// The input for the <see cref="FiniteUndirectedGraphOperation" /> operation.
+    /// </summary>
     public struct FiniteUndirectedGraphOperationIn { }
+    /// <summary>
+    /// The output of the <see cref="FiniteUndirectedGraphOperation" /> operation.
+    /// </summary>
     public struct FiniteUndirectedGraphOperationOut
     {
+        /// <summary>
+        /// The generated synthetic graph.
+        /// </summary>
         public Graph Graph { get; set; }
     }
 
@@ -21,9 +31,13 @@ namespace CartaCore.Operations
         FiniteUndirectedGraphOperationOut
     >
     {
+        /// <inheritdoc />
         public override Task<FiniteUndirectedGraphOperationOut> Perform(FiniteUndirectedGraphOperationIn input)
         {
-            return Task.FromResult(new FiniteUndirectedGraphOperationOut() { Graph = new FiniteUndirectedGraph() });
+            return Task.FromResult(
+                new FiniteUndirectedGraphOperationOut()
+                { Graph = new FiniteUndirectedGraph(new FiniteUndirectedGraphParameters()) }
+            );
         }
     }
 }
