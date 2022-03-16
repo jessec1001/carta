44d6e8ef7efd7694ae112f24a60b4b673c0ddd3b
jessec1001 <jessecorralejr@gmail.com>
2022-03-16 06:20:14 -0600

    fix: Submitting missing temporary test class

---
+++ CartaWeb/Models/DocumentItem/TestItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Text.Json.Serialization;
+using CartaCore.Operations;
+using CartaCore.Persistence;
+
+
+namespace CartaWeb.Models.DocumentItem
+{
+    public class TestItem : Item
+    {
+        public TestItem() { }
+
+        public TestItem(string secretId, string testId)
+            : base(testId, secretId) { }
+
+        [Secret]
+        public Dictionary<string, object> SecretValues { get; set; }
+
+        public string SomeOtherProperty { get; set; }
+
+        /// <inheritdoc />
+        [JsonIgnore]
+        public override string PartitionKeyPrefix => "TEST#";
+        /// <inheritdoc />
+        [JsonIgnore]
+        public override string SortKeyPrefix => "SECRET#";
+    }
+}
