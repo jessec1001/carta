9cfdd59127524e791f4a9727cdd4156012ad67de
jessec1001 <jessecorralejr@gmail.com>
2022-04-10 16:22:23 -0600

    hotfix: Uncommented pipenv installation as this fails unexpectedly during the Elastic Beanstalk deployment, as a temporary workaround for issue CU-2bfqpc9

--- .platform/hooks/postdeploy/00_python_packages.sh
+++ .platform/hooks/postdeploy/00_python_packages.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
-
-# For logging purposes, make sure Python3 and Pip3 are installed.
-pwd
-python3 --version
-pip3 --version
-
-# Install pipenv.
-sudo -H -u webapp pip3 install --user pipenv
-
-# Change to the execution directory.
-cd /var/app/current
-
-# Install Python packages.
-# After this point, you can run Python scripts with "pipenv run python3 script.py".
-sudo -H -u webapp /home/webapp/.local/bin/pipenv install numpy
+##!/bin/bash
+#
+## For logging purposes, make sure Python3 and Pip3 are installed.
+#pwd
+#python3 --version
+#pip3 --version
+#
+## Install pipenv.
+#sudo -H -u webapp pip3 install --user pipenv
+#
+## Change to the execution directory.
+#cd /var/app/current
+#
+## Install Python packages.
+## After this point, you can run Python scripts with "pipenv run python3 script.py".
+#sudo -H -u webapp /home/webapp/.local/bin/pipenv install numpy
