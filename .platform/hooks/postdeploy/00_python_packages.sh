8f70a1195ac2d13d3c9aeb28b9d03f61a65bd643
jessec1001 <jessecorralejr@gmail.com>
2022-04-21 10:46:17 -0600

    hotfix: Resolve staging deployment error of executing a script with no shell directive (so latest development branch will be available in staging)

--- .platform/hooks/postdeploy/00_python_packages.sh
+++ .platform/hooks/postdeploy/00_python_packages.sh
@@ -1,10 +1,10 @@
-##!/bin/bash
-#
-## For logging purposes, make sure Python3 and Pip3 are installed.
-#pwd
-#python3 --version
-#pip3 --version
-#
+#!/bin/bash
+
+# For logging purposes, make sure Python3 and Pip3 are installed.
+pwd
+python3 --version
+pip3 --version
+
 ## Install pipenv.
 #sudo -H -u webapp pip3 install --user pipenv
 #
